49ec5ae12a7f24d7eecb1b2cb490ab304ec11945
Steven Erik Martijn Roest <steven.roest@student.aventus.nl>
2024-10-01 11:13:56 +0200

    First version API, added sign_in and sign_up, with POST values

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
-const port = 3306;
-require('dotenv').config()
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const port = 3000;
 
+require('dotenv').config();
+app.use(bodyParser.json());
 
 var db = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -14,19 +17,62 @@ var db = mysql.createConnection({
 
 db.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
+  console.log("Connected with the database!");
 });
 
-app.get('/', (req, res) => {
-    db.query("SELECT * FROM user", function (err, result, fields) {
-      if (err) throw err;
-      res.send(result);
-    });
-    // res.send('To get all users, go to link /users')
+app.get('/', (req, res) => { // alle endpoints teruggeven
+  res.send(`FITWAVE_API
+    ----------------------
+    Endpoints:
+    /users (Get all users from table users)
+    /sign_in (Check if user exists and password is correct, then give auth)
+    /sign_up (Create account, with username, email and password (hashed))`);
 });
 
-app.get('/users', (req, res) => {
+app.get('/users', (req, res) => { // Alle users ophalen
+  db.query("SELECT * FROM user", function (err, result, fields) { // user query sql
+    if (err) throw err;
+    res.send(result);
+  });
+});
 
+app.post('/sign_in', (req, res) => {
+  const { username, password } = req.body; // request body ophalen
+
+  db.query("SELECT * FROM user", async function (err, users, fields) { // alle users ophalen database
+    if (err) throw err;
+
+    const user = users.find(u => u.username === username); // bekijken of user bestaat, alle userinfo zetten in user const
+    if(!user) {
+      return res.status(400).json({ message: 'Gebruiker niet gevonden' });
+    }
+
+    const isPassCorrect = await bcrypt.compare(password, user.password); // controleren password met bcrypt
+    if(!isPassCorrect) {
+      return res.status(400).json({ message: 'Ongeldig wachtwoord' });
+    }
+
+    res.status(200).json({ auth: true, user_id: user.id }); // authenticated, teruggeven user.id
+  });
+});
+
+app.post('/sign_up', async (req, res) => {
+  const { username, email, password } = req.body; // request body ophalen
+
+  if(!username || !email || !password) { // Checken of alle values zijn opgehaalt of bestaan
+   return res.status(400).json({message: "Er mist een value, hierdoor is het script gestopt."});
+  }
+
+  // Wachtwoorden hashen met bcrypt
+  const hash_salt = await bcrypt.genSalt(10);
+  const password_hash = await bcrypt.hash(password, hash_salt); 
+
+  // Gebruiker toevoegen aan user tabel
+  var insert_user = "INSERT INTO user (username, email, password) VALUES (?, ?, ?)";
+  db.query(insert_user, [username, email, password_hash], function (err, result) {
+    if (err) throw err;
+    res.status(201).json({ auth: true, user_id: result.insertId });
+  });
 });
 
 app.listen(port, () => {
